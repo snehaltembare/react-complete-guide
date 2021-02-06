ceef893c30363d455dc1b7b0d18b5f0bae828bbf
snehaltembare <snehal.tembare@synerzip.com>
2021-02-06 20:19:29 +0530

    Working with CSS Modules

--- src/App.js
+++ src/App.js
@@ -1,19 +1,6 @@
 import React, { Component } from 'react';
-import './App.css';
 import Person from './Person/Person';
-import styled from 'styled-components';
-const StyledButton = styled.button`
-background-color: ${props => props.alt ? 'red' : 'green'};
-color: white;
-font: inherit;
-border: 1px solid blue;
-padding: 8px;
-cursor: pointer;
-
-&:hover {
-  background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};;
-  color: black;
-}`;
+import classes from './App.css'
 class App extends Component {
   state = {
     persons: [
@@ -48,6 +35,7 @@ class App extends Component {
   };
   render() {
     let persons = null;
+    let btnClass = [classes.Button];
     if (this.state.showPersons) {
       persons = (<div>
         {this.state.persons.map((person, index) => {
@@ -61,25 +49,20 @@ class App extends Component {
        </Person>
         })}
       </div>);
-      // style.backgroundColor = 'red';
-      // style[':hover'] = {
-      //   backgroundColor: 'salmon',
-      //   color: 'black'
-      // };
+      btnClass.push(classes.Red);
     }
-
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red')
+      assignedClasses.push(classes.red)
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold')
+      assignedClasses.push(classes.bold)
     }
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>I am React app</h1>
-        <p className={classes.join(' ')}>This is really working!!</p>
-        <StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler}>Toggle Persons</StyledButton>
+        <p className={assignedClasses.join(' ')}>This is really working!!</p>
+        <button className={btnClass.join(' ')} onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
     );
